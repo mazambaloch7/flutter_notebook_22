4985ada5a0ee93acc86c334e86bee4762327d5a2
AzamKhan <30676824+mazambaloch7@users.noreply.github.com>
2022-10-07 03:53:54 -0700

    Part 2 of Event App Flutter
    Second part 2 of the even app in flutter

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # flutter_notebook_22_ui
  
+this is part 2 of this series
+
+
+thanks for you kind response and time feedback...see you in next ....
--- lib/ep_1288_event_booking_app/src/ui/event_booking_home_page.dart
+++ lib/ep_1288_event_booking_app/src/ui/event_booking_home_page.dart
@@ -84,6 +84,9 @@ CircleAvatar(),
                     fontSize: 18,
                     fontWeight: FontWeight.bold
                   ),),
+
+                  SizedBox( height: 8),
+
                   Row(
                     children: [
                       Icon(Icons.location_on,
@@ -101,8 +104,19 @@ CircleAvatar(),
 
             Container(
               margin: EdgeInsets.symmetric(horizontal: 16,vertical: 0),
-              height: 72,
-              color: Colors.white,
+              height: 54,
+              decoration:  BoxDecoration(
+                color: Colors.white,
+                borderRadius:  BorderRadius.circular(8.0),
+              ),
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: "Seach Events",
+                  border: InputBorder.none,
+                  suffixIcon: Icon(Icons.search),
+                ),
+              ),
             ),
 
             SizedBox(
@@ -115,7 +129,158 @@ CircleAvatar(),
                 borderRadius: BorderRadius.only(
                   topRight: Radius.circular(16),
                   topLeft: Radius.circular(16)
-                )
+                ),
+              ),
+              child:  SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 16.0, right: 16),//past copy that line
+
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+
+                     Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text('Category', style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),),
+
+                            TextButton(onPressed: (){}, child: Text('See All'),)
+                          ],
+                        ),
+
+
+
+
+                      Container(
+                        margin: EdgeInsets.only(left: 6),
+                        height: 54,
+
+                        child: ListView.builder(
+                            itemCount: 10,
+                            scrollDirection: Axis.horizontal,
+                            itemBuilder: (context, index) {
+                         return Container(
+                           margin: EdgeInsets.only(right: 16),
+                            padding: EdgeInsets.symmetric(
+                              horizontal: 8,vertical: 2
+                            ),
+                            decoration: BoxDecoration(
+                              color: Colors.indigo[50],
+                              borderRadius: BorderRadius.circular(24),
+                            ),
+                            child: Row(
+                              children: [
+                                CircleAvatar(
+                                  backgroundColor: Colors.white,
+                                  child: Icon(Icons.music_note,color: Colors.amber,),
+                                ),
+                                SizedBox(width: 8,),
+                                Text('Music')
+
+                              ],
+                            ),
+                          );
+
+                        }
+
+                        ),
+                      ),
+
+
+                      SizedBox(
+                        height: 10 ,
+                      ),
+
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('Newest Event', style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 22,
+                          ),),
+
+                          TextButton(onPressed: (){}, child: Text('See All'),)
+                        ],
+                      ),
+
+
+
+
+                      Container(
+                        margin: EdgeInsets.only(left: 6),
+                        height:  240,
+
+                        child: ListView.builder(
+                            itemCount: 10,
+                            scrollDirection: Axis.horizontal,
+                            itemBuilder: (context, index) {
+                              return Container(
+                                width: 200,
+                                margin: EdgeInsets.only(right: 15),
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 8,vertical: 2
+                                ),
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(24),
+                                ),
+
+
+                             /*   child: Row(
+                                  children: [
+                                    CircleAvatar(
+                                      backgroundColor: Colors.white,
+                                      child: Icon(Icons.music_note,color: Colors.amber,),
+                                    ),
+                                    SizedBox(width: 8,),
+                                    Text('Music')
+
+                                  ],
+                                ),*/
+                              );
+
+                            }
+
+                        ),
+                      ),
+
+                      SizedBox(
+                        height: 10 ,
+                      ),
+
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('Treading Event', style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 22,
+                          ),),
+
+                          TextButton(onPressed: (){}, child: Text('See All'),)
+                        ],
+                      ),
+
+
+                      ...List.generate(4, (index) => Container(
+                        height: 120,
+                        margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                        decoration: const BoxDecoration(
+                          color: Colors.white
+                        ),
+
+                      ))
+
+
+                      //
+
+                    ],
+
+
+                  ),
+                ),
               ),
               
             )),
