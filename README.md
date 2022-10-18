2d48311ddba2762713b0af44afee654c2e686f34
AzamKhan <30676824+mazambaloch7@users.noreply.github.com>
2022-10-18 09:30:52 -0700

    SplashScreen
    Splash Screen

--- README.md
+++ README.md
@@ -4,3 +4,14 @@ this is part 2 of this series
 
 
 thanks for you kind response and time feedback...see you in next ....
+
+
+ep_1289
+in this video I'm going to create the splash sceeen of the app in the few steps
+lets start
+
+That is all the code
+
+Thanks for watching
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,38 @@
 // All the code clear
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_22/ep_1289_splashscreen/splash_screen.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 import 'ep_1288_event_booking_app/event_booking_app.dart';
 
 main(){
   WidgetsFlutterBinding.ensureInitialized();
   runApp(
-    ProviderScope(child: EventBookingApp()),
+    ProviderScope(child: App()),
   );
+}
+
+
+
+
+
+class App extends StatelessWidget {
+
+  App({Key? key}) : super(key: key);
+
+  final _router = GoRouter(routes: [
+    GoRoute(path: "/",
+        builder: (context, state) => SplashScreen()),
+  ]);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routerConfig: _router,
+      theme: ThemeData.light().copyWith(
+          scaffoldBackgroundColor: Colors.orange[100]
+      ),
+    );
+  }
 }
